180e08e28e694c87e12d525a82c7012f9d5800b0
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-11-23 09:51:28 +0100

    Fixed some minor error to the todo creation

--- style/script.js
+++ style/script.js
@@ -61,10 +61,13 @@ const daysOfMonth = [
     ];
 
 
+/** todo array that stores the todos in an object
+ *  Items in object, todo and date
+ *  Date is the common denominator between the todo array and calender array
+ */
 let todoArray = [];
 
-console.log(todoArray)
-
+/** This function pushes the user input to the todo array */
 function pushTodoArray() {
     
     todoArray.push({todo:userInputValue.value, date:todoDate.value})
@@ -72,36 +75,71 @@ function pushTodoArray() {
     printTodos()
 }
 
+/** This function prints the todos on screen, when removed from array it also removes from screen */
 function printTodos() {
 
+    // Cleares the todoList div of todos that are ereased from todo array
     todoList.innerHTML = "";
 
+    // A loop to print out the todo, and paragraph with the todo and one button to erease the todo from array
     for (let i = 0; i < todoArray.length; i++) {
+
+        // Creates an varible to the array for use on the created paragraph
+        const todoData = todoArray[i];
 
         const todoList = document.querySelector('#todoList');
 
-        const todoDeleteBtn = document.createElement('button');               
-
-        // Create the container of the todo
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item');
-
+        const todoDeleteBtn = document.createElement('button');        
         const todoContent = document.createElement('p');
-        todoContent.classList.add('item-text');
-        todoItem.appendChild(todoContent)
-
-        todoContent.innerText = userInputValue.value;
-        todoDeleteBtn.classList.add('delete-btn');
-        todoDeleteBtn.innerText = 'X';
-        todoItem.appendChild(todoDeleteBtn)
 
         todoDeleteBtn.addEventListener('click', () => {
             todoArray.splice(i,1);
             printTodos();
-        })        
+        })
 
+        // Creates the container of the todo
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+
+        // Creates the paragraph
+        todoContent.innerText = todoData.todo;
+        todoContent.classList.add('item-text');
+        todoItem.appendChild(todoContent)
+
+        // Creates the button
+        todoDeleteBtn.innerText = 'X';
+        todoItem.appendChild(todoDeleteBtn)
+        todoDeleteBtn.classList.add('delete-btn');
+
+        // Appends the todo item to the todoList div
         todoList.appendChild(todoItem);
 
+        // Erease old userinput todo
         userInputValue.value = "";
     }
 }
+
+
+// Function to append a new todo to the sidebar
+// function addTodo() {
+
+//     // Create the container of the todo
+//     const todoItem = document.createElement('div');
+//     todoItem.classList.add('todo-item');
+
+//     const todoContent = document.createElement('p');
+//     todoContent.classList.add('item-text');
+//     todoContent.innerText = userInputValue.value;
+//     todoItem.appendChild(todoContent)
+
+//     const todoDeleteBtn = document.createElement('button');
+//     todoDeleteBtn.innerText = 'X';
+//     todoDeleteBtn.classList.add('delete-btn');
+//     todoItem.appendChild(todoDeleteBtn)
+
+//     todoContainer.appendChild(todoItem);
+
+//     userInputValue.value = "";
+
+//     console.log(userInputValue.value)
+// }
