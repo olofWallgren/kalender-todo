598b50c7ad5e0ceead1c4e862a1a01a33746e930
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-11-30 13:31:26 +0100

    Changed order and cleaned script files

--- script/todos.js
+++ script/todos.js
@@ -1,32 +1,3 @@
-window.addEventListener('load', startProgram);
-function startProgram() {
-    addEventListeners()
-    startTime()
-    createArray()
-    addEmptyDates()
-}
-function addEventListeners() {
-
-   // populateTodoArray()
-  //  deleteTodoLS()   
-
-
-};
-
-// global variables
-
-const createBtn = document.querySelector('#createBtn');
-const userInputValue = document.querySelector('#todoInput');
-const todoDate = document.querySelector('#todoDate');
-const alertText = document.querySelector('.alert-message');
-
-// Eventlisteners
-createBtn.addEventListener('click', pushTodoArray);
-
-
-
-
-
 /** todo array that stores the todos in an object
  *  Items in object, todo and date
  *  Date is the common denominator between the todo array and calender array
@@ -82,40 +53,28 @@ function pushTodoArray() {
     } else {
         userInputValue.style.border = '2px solid black';
         alertText.innerText = '';
+
         todoArray.push({ todo: userInputValue.value, date: todoDate.value })
-        
-        console.log(todosDate)
-        console.log(countTodos)
 
         printTodos()
-    }
-
-    
-
+    }   
 }
 
 /** This function prints the todos on screen, when removed from array it also removes from screen */
 function printTodos() {   
-
-
     // Cleares the todoList div of todos that are ereased from todo array
     todoList.innerHTML = "";
 
-    // TODO: OLOF
     // renar todo-texten i calender-item divven
     clearTodoText()
     
     // A loop to print out the todo, and paragraph with the todo and one button to erease the todo from array
     for (let i = 0; i < todoArray.length; i++) {       
         todoSum()
-        // TODO: NICKLAS
         for (let i = 0; i < countTodos.length; i++) {
             
             addTodoToDate(countTodos[i])
         }
-
-        // TODO: OLOF
-        //addTodoToDate(todoArray[i])
 
         // Creates an varible to the array for use on the created paragraph
         const todoData = todoArray[i];
@@ -158,9 +117,6 @@ function printTodos() {
 
 }
 
-// TODO: NICKLAS
-// skapar todo-texten i calender-item divven
-
 function addTodoToDate(todo) {
 
     //clearTodoText()
@@ -176,27 +132,6 @@ function addTodoToDate(todo) {
 
 }
 
-
-
-// TODO: OLOF
-// skapar todo-texten i calender-item divven
-/*
-function addTodoToDate(todo) {
-    let dateItem = dateArray.filter(item => {
-        
-        return item.dateString == todo.date;
-      
-    });
-   
-
-    let dateDiv = document.getElementById(dateItem[0].index)
-    node = document.createElement("p");
-    node.innerText = todo.todo;
-    dateDiv.appendChild(node);
-
-}
-*/
-
 //rensar texten i calender-items divven
 function clearTodoText() {
     document.querySelectorAll(".calender-item").forEach(item => {
@@ -205,62 +140,3 @@ function clearTodoText() {
         })
     })
 }
-
-// Save to local storage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // 
-
-
-
-// Function to append a new todo to the sidebar
-// function addTodo() {
-/*
-function saveTodoToLS() {
-
-
-    let todoToString = JSON.stringify(todoArray)
-    // console.log(todoToString)
-
-    localStorage.setItem('todo', todoToString)         
-}
-
-function populateTodoArray() {
-
-    if (localStorage.getItem('todo') === null) {
-
-    }
-    else {
-    let todoBack = JSON.parse(localStorage.getItem('todo'))
-    todoArray = todoBack;
-    console.log(todoBack)
-
-    printTodos();
-    }
-}
-*/
-/** Function to remove one todo from local storage and populate it with the rest of todoArray */
-/*
-function deleteTodoLS() {
-
-    // Clean the local storage
-    localStorage.clear();
-
-    // A loop to send every todo from the todoArray to local storage
-    for ( i = 0; i < todoArray.length; i++) {
-        saveTodoToLS()
-    }
-}
-*/
